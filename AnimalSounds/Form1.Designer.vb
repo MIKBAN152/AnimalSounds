4dbbdd6eca28b7e73970cdec0e62fe7bd627b99c
Miguel Bravo <mikban152@gmail.com>
2020-03-27 09:56:58 -0500

    full commit

---
+++ AnimalSounds/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btn_play = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.btn_s1 = New System.Windows.Forms.Button()
+        Me.pnl_sounds = New System.Windows.Forms.Panel()
+        Me.btn_s6 = New System.Windows.Forms.Button()
+        Me.btn_s5 = New System.Windows.Forms.Button()
+        Me.btn_s4 = New System.Windows.Forms.Button()
+        Me.btn_s3 = New System.Windows.Forms.Button()
+        Me.btn_s2 = New System.Windows.Forms.Button()
+        Me.pnl_sounds.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btn_play
+        '
+        Me.btn_play.Location = New System.Drawing.Point(200, 124)
+        Me.btn_play.Name = "btn_play"
+        Me.btn_play.Size = New System.Drawing.Size(116, 42)
+        Me.btn_play.TabIndex = 0
+        Me.btn_play.Text = "PLAY"
+        Me.btn_play.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label1.Location = New System.Drawing.Point(40, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(428, 42)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "CIENCIA AL RESCATE"
+        '
+        'btn_s1
+        '
+        Me.btn_s1.BackColor = System.Drawing.Color.DarkSlateGray
+        Me.btn_s1.ForeColor = System.Drawing.Color.White
+        Me.btn_s1.Location = New System.Drawing.Point(45, 31)
+        Me.btn_s1.Name = "btn_s1"
+        Me.btn_s1.Size = New System.Drawing.Size(54, 34)
+        Me.btn_s1.TabIndex = 3
+        Me.btn_s1.Text = "S1"
+        Me.btn_s1.UseVisualStyleBackColor = False
+        '
+        'pnl_sounds
+        '
+        Me.pnl_sounds.BackColor = System.Drawing.Color.GreenYellow
+        Me.pnl_sounds.Controls.Add(Me.btn_s6)
+        Me.pnl_sounds.Controls.Add(Me.btn_s5)
+        Me.pnl_sounds.Controls.Add(Me.btn_s4)
+        Me.pnl_sounds.Controls.Add(Me.btn_s3)
+        Me.pnl_sounds.Controls.Add(Me.btn_s2)
+        Me.pnl_sounds.Controls.Add(Me.btn_s1)
+        Me.pnl_sounds.Location = New System.Drawing.Point(132, 203)
+        Me.pnl_sounds.Name = "pnl_sounds"
+        Me.pnl_sounds.Size = New System.Drawing.Size(250, 188)
+        Me.pnl_sounds.TabIndex = 4
+        Me.pnl_sounds.Visible = False
+        '
+        'btn_s6
+        '
+        Me.btn_s6.BackColor = System.Drawing.Color.DarkGoldenrod
+        Me.btn_s6.ForeColor = System.Drawing.Color.White
+        Me.btn_s6.Location = New System.Drawing.Point(270, 112)
+        Me.btn_s6.Name = "btn_s6"
+        Me.btn_s6.Size = New System.Drawing.Size(54, 34)
+        Me.btn_s6.TabIndex = 8
+        Me.btn_s6.Text = "S6"
+        Me.btn_s6.UseVisualStyleBackColor = False
+        Me.btn_s6.Visible = False
+        '
+        'btn_s5
+        '
+        Me.btn_s5.BackColor = System.Drawing.Color.DarkOliveGreen
+        Me.btn_s5.ForeColor = System.Drawing.Color.White
+        Me.btn_s5.Location = New System.Drawing.Point(270, 31)
+        Me.btn_s5.Name = "btn_s5"
+        Me.btn_s5.Size = New System.Drawing.Size(54, 34)
+        Me.btn_s5.TabIndex = 7
+        Me.btn_s5.Text = "S5"
+        Me.btn_s5.UseVisualStyleBackColor = False
+        Me.btn_s5.Visible = False
+        '
+        'btn_s4
+        '
+        Me.btn_s4.BackColor = System.Drawing.Color.Indigo
+        Me.btn_s4.ForeColor = System.Drawing.Color.White
+        Me.btn_s4.Location = New System.Drawing.Point(158, 112)
+        Me.btn_s4.Name = "btn_s4"
+        Me.btn_s4.Size = New System.Drawing.Size(54, 34)
+        Me.btn_s4.TabIndex = 6
+        Me.btn_s4.Text = "S4"
+        Me.btn_s4.UseVisualStyleBackColor = False
+        '
+        'btn_s3
+        '
+        Me.btn_s3.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btn_s3.ForeColor = System.Drawing.Color.White
+        Me.btn_s3.Location = New System.Drawing.Point(45, 112)
+        Me.btn_s3.Name = "btn_s3"
+        Me.btn_s3.Size = New System.Drawing.Size(54, 34)
+        Me.btn_s3.TabIndex = 5
+        Me.btn_s3.Text = "S3"
+        Me.btn_s3.UseVisualStyleBackColor = False
+        '
+        'btn_s2
+        '
+        Me.btn_s2.BackColor = System.Drawing.Color.Maroon
+        Me.btn_s2.ForeColor = System.Drawing.Color.White
+        Me.btn_s2.Location = New System.Drawing.Point(158, 31)
+        Me.btn_s2.Name = "btn_s2"
+        Me.btn_s2.Size = New System.Drawing.Size(54, 34)
+        Me.btn_s2.TabIndex = 4
+        Me.btn_s2.Text = "S2"
+        Me.btn_s2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Orange
+        Me.ClientSize = New System.Drawing.Size(505, 459)
+        Me.Controls.Add(Me.btn_play)
+        Me.Controls.Add(Me.pnl_sounds)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "SOUND FLOOR"
+        Me.pnl_sounds.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_play As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents btn_s1 As Button
+    Friend WithEvents pnl_sounds As Panel
+    Friend WithEvents btn_s6 As Button
+    Friend WithEvents btn_s5 As Button
+    Friend WithEvents btn_s4 As Button
+    Friend WithEvents btn_s3 As Button
+    Friend WithEvents btn_s2 As Button
+End Class
---
+++ AnimalSounds/Form1.vb
@@ -0,0 +1,147 @@
+﻿Imports System.Xml
+Imports System.Data
+Imports System.Xml.Schema
+Imports System.Text
+Imports System.Xml.XmlReader
+Imports System.IO
+Public Class Form1
+
+    Dim soundPath As String = ""
+    Dim startgame As Boolean = False
+    Dim serial_port As String = ""
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_play.Click
+        If startgame Then
+            startgame = False
+            btn_play.Text = "MANUAL"
+            pnl_sounds.Visible = False
+            thegame()
+        Else
+            startgame = True
+            btn_play.Text = "AUTO"
+            pnl_sounds.Visible = True
+            'thegame()
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        soundPath = System.Windows.Forms.Application.StartupPath & "\sonidos\"
+        Dim file_name As String = System.Windows.Forms.Application.StartupPath & "\floor_cfg.xml"
+        Dim doc As New XmlDocument()
+
+        Try
+            'Se carga el documento xml
+            doc.Load(file_name)
+
+            'Dim xmllist As XmlNodeList = doc.SelectNodes("config").Item(0).ChildNodes
+            Dim xmlserialport As String = doc.SelectNodes("config").Item(0).ChildNodes.Item(0).InnerText.ToString
+            serial_port = xmlserialport
+        Catch ex As Exception
+            MsgBox("Cannot setup Serial Port")
+        End Try
+
+    End Sub
+
+
+
+    Private Sub playSound(ByVal nmr_son As Integer)
+
+        Select Case nmr_son
+            Case 1
+                My.Computer.Audio.Play(soundPath & "sonido1.wav")
+            Case 2
+                My.Computer.Audio.Play(soundPath & "sonido2.wav")
+            Case 3
+                My.Computer.Audio.Play(soundPath & "sonido3.wav")
+            Case 4
+                My.Computer.Audio.Play(soundPath & "sonido4.wav")
+            Case 5
+                My.Computer.Audio.Play(soundPath & "sonido5.wav")
+            Case 6
+                My.Computer.Audio.Play(soundPath & "sonido6.wav")
+            Case Else
+                'My.Computer.Audio.Play(soundPath & "Alarm01.wav")
+        End Select
+
+    End Sub
+
+    Private Sub btn_s1_Click(sender As Object, e As EventArgs) Handles btn_s1.Click
+        If startgame Then
+            playSound(1)
+        End If
+    End Sub
+
+    Private Sub btn_s2_Click(sender As Object, e As EventArgs) Handles btn_s2.Click
+        If startgame Then
+            playSound(2)
+        End If
+    End Sub
+
+    Private Sub btn_s3_Click(sender As Object, e As EventArgs) Handles btn_s3.Click
+        If startgame Then
+            playSound(3)
+        End If
+    End Sub
+
+    Private Sub btn_s4_Click(sender As Object, e As EventArgs) Handles btn_s4.Click
+        If startgame Then
+            playSound(4)
+        End If
+    End Sub
+
+    Private Sub btn_s5_Click(sender As Object, e As EventArgs) Handles btn_s5.Click
+        If startgame Then
+            playSound(5)
+        End If
+    End Sub
+
+    Private Sub btn_s6_Click(sender As Object, e As EventArgs) Handles btn_s6.Click
+        If startgame Then
+            playSound(6)
+        End If
+    End Sub
+
+    Private Sub thegame()
+
+        While (Not startgame)
+            Application.DoEvents()
+            My.Application.Log.WriteEntry("cycle")
+            Dim opt As Integer = 0
+                Dim Data As String = ""
+                Dim Data_clean As String = ""
+            Try
+                Me.SerialPort1.Close()
+                Me.SerialPort1.PortName = serial_port '"COM3"
+                Me.SerialPort1.BaudRate = 9600
+                Me.SerialPort1.Parity = System.IO.Ports.Parity.None
+                Me.SerialPort1.DataBits = 8
+                SerialPort1.StopBits = System.IO.Ports.StopBits.One
+                'SerialPort1.Handshake = Handshake.None
+                SerialPort1.Encoding = System.Text.Encoding.Default
+                SerialPort1.ReadTimeout = 3000
+                Me.SerialPort1.Open()
+                'Me.SerialPort1.WriteLine("R:")
+
+                'Threading.Thread.Sleep(100)
+
+                Try
+                    Data = Me.SerialPort1.ReadTo("?")
+                    My.Application.Log.WriteEntry(Data)
+                    opt = CInt(Data)
+                    playSound(opt)
+                Catch ex As Exception
+                    My.Application.Log.WriteEntry("no message")
+                    'Exit While
+                End Try
+
+                Me.SerialPort1.Close()
+
+            Catch ex As Exception
+                My.Application.Log.WriteEntry("setting up error")
+                'Exit While
+            End Try
+            Threading.Thread.Sleep(200)
+        End While
+    End Sub
+
+End Class
